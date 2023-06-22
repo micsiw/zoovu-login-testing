970ae116439d8f71a7efb4b21c2e28a023136393
micsiw <michalsiwanowicz@gmail.com>
2023-06-22 02:14:33 +0200

    created template and added dependencies

---
+++ .mocharc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reporter: "node_modules/mochawesome",
+  "reporter-option": [
+    "overwrite=true",
+    "reportTitle=Report summary",
+    "reportFilename=test-report",
+    "reportDir=[cwd]/report",
+    "autoOpen=true",
+  ],
+};
